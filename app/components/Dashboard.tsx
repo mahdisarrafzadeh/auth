ad83e2bd4a66400f05210ac5361ecd6a45dc5a24
mohammad.sarraf <mohammad.sarraf99@gmail.com>
2025-07-06 22:26:34 +0330

    init

---
+++ app/components/Dashboard.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      {/* Add your dashboard content here */}
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ app/components/Login.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Add your login form elements here */}
+    </div>
+  );
+};
+
+export default Login;
